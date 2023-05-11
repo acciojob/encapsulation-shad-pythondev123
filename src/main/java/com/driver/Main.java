b0ba41d01ecb1942489e3fbe4d5ec5810e29f6bf
shad <princekhan785@gmail.com>
2023-05-12 04:25:55 +0530

    tried setting a value to name and printing it. got an error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-      RWOnly ans= new RWOnly();
-//      ans.name= "shad";
-//      System.out.println();
+      RWOnly ans= new RWOnly("shad");
+
+      System.out.println(ans.name);
   }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,8 @@ package com.driver;
 
 public class RWOnly {
     private String name;
+    public RWOnly(String name){
+        this.name= name;
+    }
+
 }
