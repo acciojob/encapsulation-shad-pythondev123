db729462a329c4473c333f782ab2931036203d2a
shad <princekhan785@gmail.com>
2023-05-16 05:37:06 +0530

    removed constructor from RWOnly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args){
-      RWOnly ans= new RWOnly("shad");
+      RWOnly ans= new RWOnly();
       ans.setName("shad");
       System.out.println(ans.getName());
   }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,9 +2,7 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    public RWOnly(String name){
-        this.name= name;
-    }
+
     public void setName(String name){
         this.name= name;
     }
